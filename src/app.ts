b30a1e82e5e55c79e772a5c26bc92543deedc9cd
habib-153 <h.r.sihab155@gmail.com>
2024-05-25 08:38:19 +0600

    user , middleware, utils and module complete

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { StudentRoutes } from './app/modules/student/student.route';
+import { userRoutes } from './app/modules/user/user.route';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import router from './app/routes';
 
 const app: Application = express();
 //const port = 3000
@@ -9,11 +13,14 @@ app.use(express.json());
 app.use(cors());
 
 //  application routes
-app.use('/api/v1/students', StudentRoutes)
+app.use('/api/v1', router)
 
 app.get('/', (req: Request, res: Response) => {
   //const a = 10;
   res.send("Hello");
 });
+
+app.use(globalErrorHandler)
+app.use(notFound)
 
 export default app;
