e246429d2142eb225a660b499f34bfec3dabe208
habib-153 <h.r.sihab155@gmail.com>
2024-06-01 22:20:52 +0600

    bug fixed

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -1,6 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TAcademicDepartment } from "./academicDepartment.interface";
 import httpStatus from "http-status";
+import AppError from "../../errors/AppError";
 
 const academicDepartmentSchema =  new Schema<TAcademicDepartment>(
     {
@@ -19,33 +20,33 @@ const academicDepartmentSchema =  new Schema<TAcademicDepartment>(
     }
 )
 
-// academicDepartmentSchema.pre('save', async function (next){
-//     const isDepartmentExist = await AcademicDepartment.findOne({
-//         name: this.name
-//     })
+academicDepartmentSchema.pre('save', async function (next){
+    const isDepartmentExist = await AcademicDepartment.findOne({
+        name: this.name
+    })
 
-//     if(isDepartmentExist){
-//         throw new AppError(
-//             httpStatus.NOT_FOUND,
-//             'This Department is already exits'
-//         )
-//     }
-//     next()
-// })
+    if(isDepartmentExist){
+        throw new AppError(
+            httpStatus.NOT_FOUND,
+            'This Department is already exits'
+        )
+    }
+    next()
+})
 
-// academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
-//     const query = this.getQuery();
-//     const isDepartmentExist = await AcademicDepartment.findOne(query);
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+    const query = this.getQuery();
+    const isDepartmentExist = await AcademicDepartment.findOne(query);
   
-//     if (!isDepartmentExist) {
-//       throw new AppError(
-//         httpStatus.NOT_FOUND,
-//         'This department does not exist! ',
-//       );
-//     }
+    if (!isDepartmentExist) {
+      throw new AppError(
+        httpStatus.NOT_FOUND,
+        'This department does not exist! ',
+      );
+    }
   
-//     next();
-//   });
+    next();
+  });
 
 export const AcademicDepartment = model<TAcademicDepartment>(
     'AcademicDepartment', academicDepartmentSchema
