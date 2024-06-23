c8b6f1ad5324c7a3f19b93b4f38f7c8d1781c989
habib-153 <h.r.sihab155@gmail.com>
2024-06-23 21:51:23 +0600

    authorization added on user creation

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -4,23 +4,26 @@ import validateRequest from '../../middlewares/validateRequest';
 import { createStudentValidationSchema } from '../student/student.validation';
 import { createFacultyValidationSchema } from '../Faculty/faculty.validation';
 import { createAdminValidationSchema } from '../Admin/admin.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLE } from './user.constant';
 
 const router = express.Router();
 
 router.post(
-  '/create-student',
+  '/create-student', auth(USER_ROLE.admin),
   validateRequest(createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
 router.post(
-  '/create-faculty',
+  '/create-faculty', auth(USER_ROLE.admin),
   validateRequest(createFacultyValidationSchema),
   UserControllers.createFaculty,
 );
 
 router.post(
   '/create-admin',
+  // auth(USER_ROLE.admin),
   validateRequest(createAdminValidationSchema),
   UserControllers.createAdmin,
 );
