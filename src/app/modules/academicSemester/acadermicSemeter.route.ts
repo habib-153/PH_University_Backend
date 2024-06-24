8219e735fc46ac3727ebd42ee9048a45a5b01004
habib-153 <h.r.sihab155@gmail.com>
2024-06-24 21:43:22 +0600

    auth guard on academic semester

--- src/app/modules/academicSemester/acadermicSemeter.route.ts
+++ src/app/modules/academicSemester/acadermicSemeter.route.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { AcademicSemesterControllers } from './academicSemeter.controller';
 import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterValidations } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
 
 const router = express.Router();
 
 router.post(
   '/create-academic-semester',
+  auth('admin'),
   validateRequest(
     AcademicSemesterValidations.createAcademicSemesterValidationSchema,
   ),
@@ -15,6 +17,6 @@ router.post(
 
 router.get('/', AcademicSemesterControllers.getAllAcademicServices)
 router.get('/:semesterId', AcademicSemesterControllers.getSingleAcademicSemester)
-router.patch('/semesterId', validateRequest(AcademicSemesterValidations.updateAcademicSemesterValidationSchema),AcademicSemesterControllers.updateAcademicSemester)
+router.patch('/semesterId', auth('admin', 'faculty'), validateRequest(AcademicSemesterValidations.updateAcademicSemesterValidationSchema),AcademicSemesterControllers.updateAcademicSemester)
 
 export const AcademicSemesterRoutes = router;
